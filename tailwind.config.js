842cdc92265c25b2ecf81af8d83c10c4c901aacd
mani333007 <mani333007@gmail.com>
2021-04-03 18:03:09 +0530

    Added theming in tailwing.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: {
+          primary: '#432543',
+          secondary: '#6E3C6E',
+          tertiary: '#4C2A4D'
+        }
+      },
+      fontFamily: {
+        'main': ['Hind Siliguri'],
+        'secondary': ['Dancing Script']
+      }
+    },
   },
   variants: {
     extend: {},
